668ec036116dc903108ace7bc0787504d19fe451
Arif Yardım <96440900+hotbagcom@users.noreply.github.com>
2024-04-26 11:14:09 +0300

    designed state machine
    state machine commend not well defined

--- ultrasonic_distance/Test_02_ultrsonics_distance.v
+++ ultrasonic_distance/Test_02_ultrsonics_distance.v
@@ -4,25 +4,56 @@ module Test_02_ultrasonics_distance
 input clk , on_off , echo;
 output trig;
 output [6:0]ones , [6:0]tens;
-reg [11:0]waitftriger = 3'x000;
-reg [15:0]waitforecho = 4'x0001;
+reg [8:0]waitftriger = 9'x000; //50 Mhz /(10us == 100 000 Hz )  =>  500 tick   ,  2^9 = 512
+reg [18:0]waitfecho = 19'x00001;   //50 MHz *0.038  =>1 900 000 tick (4 meter),   475000 tick  (1 meter)  2^19 = 524288 
 reg [9:0]dist_mm; //2^10 =1024
 reg [6:0]digitones , [6:0]digittens;
-reg [3:0]counttrigbrust;
+reg [1:0]stage =2'b00;
+
+//trig üret
+//waitfecho arttır
+//echo geldi
+//waitftrig arttır
+
+
+
+
 //clock 50 Mhz 
 //I need to produce 10 us high 
 //I need to count reflectin distance in time
 // more than 38ms means no object nearby
 always @(posedge clk) begin;
 
-//check is wave expected means incrementing wait for echo
+if( echo == 1'b0 && waitfecho ==19'd0 ) begin  
+stage <= 2'b01;
+end 
 
+else if( echo == 1'b0 && waitftriger ==9'd0 ) begin 
+stage <= 2'b10;
+end
 
+else if( echo == 1'b1 && waitftriger ==9'd500 ) begin 
+waitforecho = 19'd1;
+end
+
+else if( echo == 1'b1 && waitfecho ==19'd475000 ) begin 
+waitforecho = 19'd1;
+end
 
 // else no wave expecting produce trig  
+if(waitforecho ==9'd0) begin 
+
+
 
 
 end
+
+case 
+
+2'b01: waitftriger = waitftriger + 1'b1;
+2'b10: waitfecho = waitfecho + 1'b1;
+2'b11: waitfecho <= 19'd0;
+2'b00: 
 
 end module
 
